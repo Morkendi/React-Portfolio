5c79b67a6db19c5d55db4248d76d0adcce67b53a
Daniel Sanchez <morkendi@gmail.com>
2023-08-08 00:04:15 -0600

    Updates

---
+++ precache-manifest.0833114bbb3df2ebc971494852d766c8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "62553bb94f44fb9db20d",
+    "url": "/React-Portfolio/static/css/main.a7468f41.chunk.css"
+  },
+  {
+    "revision": "62553bb94f44fb9db20d",
+    "url": "/React-Portfolio/static/js/main.62553bb9.chunk.js"
+  },
+  {
+    "revision": "a2fbf3d3916c22598ea1",
+    "url": "/React-Portfolio/static/css/1.5b7d95cc.chunk.css"
+  },
+  {
+    "revision": "a2fbf3d3916c22598ea1",
+    "url": "/React-Portfolio/static/js/1.a2fbf3d3.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "09cb37a5d1294d9f13346a8844e025c4",
+    "url": "/React-Portfolio/static/media/avatar.09cb37a5.jpg"
+  },
+  {
+    "revision": "368cc262ff5bdfe538fad0296ade4125",
+    "url": "/React-Portfolio/static/media/commerce_img.368cc262.png"
+  },
+  {
+    "revision": "07b4896ca61e496f84825711a1f04ed8",
+    "url": "/React-Portfolio/static/media/social-network_img.07b4896c.png"
+  },
+  {
+    "revision": "e9def05104996d6f165a7c53e7ce95b2",
+    "url": "/React-Portfolio/static/media/text-editor_img.e9def051.png"
+  },
+  {
+    "revision": "99353942e5d5032b3450fa34f9306015",
+    "url": "/React-Portfolio/static/media/sales-tracker_img.99353942.png"
+  },
+  {
+    "revision": "a50719a289ab7148d78207b4ddc09053",
+    "url": "/React-Portfolio/static/media/weather-dash_img.a50719a2.png"
+  },
+  {
+    "revision": "be0ebbb5b967752f9b7bb71242aa066d",
+    "url": "/React-Portfolio/static/media/password_img.be0ebbb5.gif"
+  },
+  {
+    "revision": "b57433f72b534eb21e11d317fcc3d760",
+    "url": "/React-Portfolio/static/media/Resume.b57433f7.pdf"
+  },
+  {
+    "revision": "3d0ec57f40d62ba6a8e0d1857bb8f7d5",
+    "url": "/React-Portfolio/static/media/bootstrap-icons.3d0ec57f.woff"
+  },
+  {
+    "revision": "a30fb81bd52143bcd4de2898422ac8b9",
+    "url": "/React-Portfolio/static/media/bootstrap-icons.a30fb81b.woff2"
+  },
+  {
+    "revision": "4256fa0d65870736d002e6d7f0a60ead",
+    "url": "/React-Portfolio/index.html"
+  }
+];
